083c0c757ad6982ef95d920a8a66ef02fa22be1a
Tony <1020070531@qq.com>
2020-01-29 19:58:00 +0800

    完成登录框

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/User.dart';
@@ -41,19 +42,82 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _buildLoginArea(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text("账号"),
-        Text("密码"),
-        RaisedButton(
-          child: const Text("登录"),
-          onPressed: _login,
+    TextStyle titleFontStyle = TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 24,
+    );
+    return AspectRatio(
+      aspectRatio: 10.0 / 9.5,
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        elevation: 20.0,
+        color: Colors.blue[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 20, 430, 20),
+              child: Text(
+                '账号',
+                textAlign: TextAlign.left,
+                style: titleFontStyle,
+              ),
+            ),
+            TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '账号',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 20, 430, 20),
+              child: Text(
+                '密码',
+                textAlign: TextAlign.left,
+                style: titleFontStyle,
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '密码',
+              ),
+            ),
+            RaisedButton(
+              child: const Text('登录'),
+              onPressed: _login,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  '还没有账号？快去',
+                  textAlign: TextAlign.left,
+                ),
+                FlatButton(
+                  child: Text('注册'),
+                  onPressed: _gotoRegister,
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  splashColor: Colors.blueAccent,
+                ),
+                Text(
+                  '一个',
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 
+  _gotoRegister() {}
   _login() {
     Provider.of<User>(context).login("用户1", "密码1");
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:problem_mgr_cli/main.dart';
+import 'package:problem_mgr_cli/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
